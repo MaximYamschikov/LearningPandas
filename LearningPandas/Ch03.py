2504e24a438ca060300c0d91a7684c9c0363501b
Maxim Yamschikov <myamschikov@gmail.com>
2024-11-18 20:46:08 +0300

    1

--- LearningPandas/Ch03.py
+++ LearningPandas/Ch03.py
@@ -1,8 +1,26 @@
+from operator import index
+
 import pandas as pd
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
 s = pd.Series(np.arange(100, 110), index = np.arange(10, 20))
-print(s[0:2])
-print(s.loc[10:12])
+
+ind = ['a','b','c','d','e']
+ind = [100, 101, 102,103, 104]
+t = pd.Series([10,11,12,13, 14])
+# t = pd.Series([10,11,12,13, 14], ind)
+# print(s[0:2])
+# print(s.loc[10:12])
+# print(s)
+# print()
+# print(s[4::])
+# print()
+# print(s[-4:-3])
+# print(s[(s > 105) & (s < 108)])
+# print((s > 105).sum())
+# print(s[(s > 105) & (s < 108)])
+# print(s[(s > 105) & (s < 108)].count())
+t.index = ind
+print(t.iloc[].index)
