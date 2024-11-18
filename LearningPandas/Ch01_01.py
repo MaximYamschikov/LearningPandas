45bb425ec62ed91ffd838e12df377c84bbc10717
Maxim Yamschikov <myamschikov@gmail.com>
2024-11-18 05:55:10 +0300

    1

--- LearningPandas/Ch01_01.py
+++ LearningPandas/Ch01_01.py
@@ -15,10 +15,27 @@ df = pd.read_csv(r'c:\ML\LearningPandas\Data\goog.csv', parse_dates=['Date'])
 dates = pd.date_range('2010-01-01','2010-01-05')
 
 temps1 = pd.Series([80,82,85,90,83], index = dates )
-temps2 = pd.Series([78,175,69,79,77])
+temps2 = pd.Series([78,175,69,79,77], index = [-1,3,40,50,9])
+# temps2 = pd.Series([78,175,69,79,77])
 
 
-print(temps1['2010-01-01'])
+# print(temps1['2010-01-01'])
+# print(temps1[['2010-01-01']])
+# print(temps1[['2010-01-01', '2010-01-02']])
+# print()
+# print(temps1[0:1])
+# # print(temps1[[0,1]])
+# print(temps1.iloc[[0,1]])
+# print(temps2[[0,1,9]])
+# print(type(temps2.values))
+print(temps2.loc[40:50])
+print(temps2.iloc[2]) ##!!!!
+print(temps2.iloc[3]) ##!!!!
+print(temps2.iloc[2:4]) ##!!!!
+# print(temps1[0:1])
+
+
+
 # print(type(df.Date[0]))
 # print(df.index)
 # print(df.columns)
--- LearningPandas/Ch03.py
+++ LearningPandas/Ch03.py
@@ -2,3 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+
+s = pd.Series(np.arange(100, 110), index = np.arange(10, 20))
+print(s[0:2])
+print(s.loc[10:12])
